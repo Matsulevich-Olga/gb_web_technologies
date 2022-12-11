ca6df9c1285e4a2509bb8e1142acdadce8eb8c14
Olga Matsulevich <flintrok@ya.ru>
2022-12-11 22:08:26 +0300

    Homework4 added

---
+++ Homework4/datainfo.php
@@ -0,0 +1,17 @@
+<?php
+   return [
+     'experience' => [
+       'job1'   => 'Front End Developer',
+       'job2'   => 'Web Developer / something.com',
+       'job3'   => 'Graphic Designer / designsomething.com',
+       'date1'   => 'Jan 2015 - Current',
+       'date2'   => 'Mar 2012 - Dec 2014',
+       'date3'   => 'Jun 2010 - Mar 2012',
+       'exp1'   => 'Lorem ipsum dolor sit amet. Praesentium magnam consectetur vel in deserunt aspernatur est reprehenderit sunt hic. Nulla tempora soluta ea et odio, unde doloremque repellendus iure, iste.',
+       'exp2'  => 'Consectetur adipisicing elit. Praesentium magnam consectetur vel in deserunt aspernatur est reprehenderit sunt hic. Nulla tempora soluta ea et odio, unde doloremque repellendus iure, iste.',
+       'exp3'    => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.'
+
+
+     ]
+];
+?>
